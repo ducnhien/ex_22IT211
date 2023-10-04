87e865cabee3aece1459086dc891e1a5455cebf5
Nhiên Ngô Văn Đức <nhienhk19@gmail.com>
2023-10-04 15:16:53 +0700

    04/10/23

--- ch11_ex1/index.php
+++ ch11_ex1/index.php
@@ -1,54 +1,77 @@
 <?php
-//get tasklist array from POST
-$task_list = filter_input(INPUT_POST, 'tasklist', 
-        FILTER_DEFAULT, FILTER_REQUIRE_ARRAY);
-if ($task_list === NULL) {
-    $task_list = array();
-    
-//    add some hard-coded starting values to make testing easier
-//    $task_list[] = 'Write chapter';
-//    $task_list[] = 'Edit chapter';
-//    $task_list[] = 'Proofread chapter';
-}
+// Start session management with a persistent cookie
+//$lifetime = 60 * 60 * 24 * 14;         // 2 weeks in seconds
+// $lifetime = 0;                        // session cookie
+$lifetime = 60 * 60 * 24 * 365 * 4;    // 3 years in seconds
+session_set_cookie_params($lifetime, '/');
+session_start();
 
-//get action variable from POST
+// Create a cart array if needed
+if (empty($_SESSION['cart12'])) { $_SESSION['cart12'] = array(); }
+
+// Create a table of products
+$products = array();
+$products['MMS-1754'] = array('name' => 'Flute', 'cost' => '149.50');
+$products['MMS-6289'] = array('name' => 'Trumpet', 'cost' => '199.50');
+$products['MMS-3408'] = array('name' => 'Clarinet', 'cost' => '299.50');
+
+// Include cart functions
+require_once('cart.php');
+
+// Get the action to perform
 $action = filter_input(INPUT_POST, 'action');
-
-//initialize error messages array
-$errors = array();
-
-//process
-switch( $action ) {
-    case 'Add Task':
-        $new_task = filter_input(INPUT_POST, 'newtask');
-        if (empty($new_task)) {
-            $errors[] = 'The new task cannot be empty.';
-        } else {
-            $task_list[] = $new_task;
-        }
-        break;
-    case 'Delete Task':
-        $task_index = filter_input(INPUT_POST, 'taskid', FILTER_VALIDATE_INT);
-        if ($task_index === NULL || $task_index === FALSE) {
-            $errors[] = 'The task cannot be deleted.';
-        } else {
-            unset($task_list[$task_index]);
-            $task_list = array_values($task_list);
-        }
-        break;
-/*
-    case 'Modify Task':
-    
-    case 'Save Changes':
-    
-    case 'Cancel Changes':
-    
-    case 'Promote Task':
-        
-    case 'Sort Tasks':
-    
-*/
+if ($action === NULL) {
+    $action = filter_input(INPUT_GET, 'action');
+    if ($action === NULL) {
+        $action = 'show_add_item';
+    }
 }
 
-include('task_list.php');
+// Add or update cart as needed
+switch($action) {
+    case 'add':
+        $product_key = filter_input(INPUT_POST, 'productkey');
+        $item_qty = filter_input(INPUT_POST, 'itemqty');
+        add_item($product_key, $item_qty);
+        include('cart_view.php');
+        break;
+    case 'update':
+        $new_qty_list = filter_input(INPUT_POST, 'newqty', FILTER_DEFAULT, FILTER_REQUIRE_ARRAY);
+        foreach($new_qty_list as $key => $qty) {
+            if ($_SESSION['cart12'][$key]['qty'] != $qty) {
+                update_item($key, $qty);
+            }
+        }
+        include('cart_view.php');
+        break;
+    case 'show_cart':
+        include('cart_view.php');
+        break;
+    case 'show_add_item':
+        include('add_item_view.php');
+        break;
+    case 'empty_cart':
+        unset($_SESSION['cart12']);
+        include('cart_view.php');
+        break;
+    case 'end_session':
+        // Clear session data from memory
+        $_SESSION = array();
+
+        // Clean up session ID
+        session_destroy();
+
+        // Delete the cookie for the session
+        $name = session_name();                // Get name of the session cookie
+        $expire = strtotime('-1 year');        // Create expiration date in the past
+        $params = session_get_cookie_params(); // Get session params
+        $path = $params['path'];
+        $domain = $params['domain'];
+        $secure = $params['secure'];
+        $httponly = $params['httponly'];
+        setcookie($name, '', $expire, $path, $domain, $secure, $httponly);
+
+        include('cart_view.php');
+        break;
+}
 ?>
